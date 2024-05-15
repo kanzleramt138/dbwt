c570dbe619454fe0aa7029d3dd98e78b95e7b6f4
josu <josuel.arz@alumni.fh-aachen.de>
2024-05-15 15:33:49 +0200

    M2 Aufgabe 1 und 3 a-e

---
+++ M2/beispiele/erstesphp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Projekt - M2</title>
+</head>
+<body>
+<?php 
+echo "Erstes PHP-Skript <br>"; 
+phpinfo(); 
+?>
+</body>
+</html>
+
