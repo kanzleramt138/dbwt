6e1c474d43b2a7b290fc7c3a9e80c7fa7538480b
rohor <robert.hormann@alumni.fh-aachen.de>
2024-05-17 17:12:07 +0200

    Test Timezone

--- beispiele/m2_7a_accesslog.php
+++ beispiele/m2_7a_accesslog.php
@@ -4,7 +4,7 @@
 - Josuel, Arz, 3307282
 -->
 <?php
-date_default_timezone_set('Europe/Berlin');
+//date_default_timezone_set('Europe/Berlin');
 $logFile = "acceslog.txt";
 
 $ip = $_SERVER['REMOTE_ADDR'];
