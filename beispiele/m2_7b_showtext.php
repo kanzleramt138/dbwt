4e6a5e0f5ac72bb56aabc1967e1415ea50448726
rohor <robert.hormann@alumni.fh-aachen.de>
2024-05-17 15:59:21 +0200

    M2 Aufgabe 7 fertig

--- beispiele/m2_7b_showtext.php
+++ beispiele/m2_7b_showtext.php
@@ -4,20 +4,37 @@
 - Josuel, Arz, 3307282
 -->
 <?php
+function searchLine($line, $searchWord) {
+    $explodedLine = explode(";", $line);
+    $cleanLine = [];
+    foreach ($explodedLine as $word) {
+        $word = trim($word);
+        $cleanLine[] = $word;
+    }
+    if (in_array($searchWord, $cleanLine)) {
+        return $cleanLine[0];
+    } else {
+        return null;
+    }
+}
+
 $fileName = "en.txt";
 
 if (isset($_GET['suche'])) {
-    $searchWord = htmlspecialchars($_GET['suche']);
+    $searchWord = $_GET['suche'];
     $fileContent = file($fileName);
-
-    $positionIndex = 0;
+    $translation = "";
     foreach ($fileContent as $line) {
-        if (in_array($searchWord, $line)) {
-            
+        $translation = searchLine($line, $searchWord);
+        if ($translation != null) {
+            break;
         }
     }
-    //if (strpos)
+    
+    if ($translation == null) {
+        echo "Es wurde keine Übersetzung für $searchWord gefunden.";
+    } else {
+        echo "Die Übersetzung von $searchWord lautet $translation.";
+    }
 }
-var_dump($fileContent);
-
 ?>
